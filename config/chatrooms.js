33b06d43b4b3de80502a5450938380da2f580e08
agung <agungputra.em@gmail.com>
2018-09-21 14:42:44 +0700

    master

---
+++ config/chatrooms.js
@@ -0,0 +1,19 @@
+module.exports = [
+  {
+    name: 'Admin Group',
+    image: 'chatrooms/valdo.jpg'
+  }
+  // {
+  //   name: 'Real Madrid',
+  //   image: 'chatrooms/M.jpg'
+  // },
+  // {
+  //   name: 'Sanctuary',
+  //   image: 'chatrooms/S.jpg'
+  // },
+  // {
+  //   name: 'Indo',
+  //   image: 'chatrooms/indo.jpg'
+  // }
+]
+
---
+++ config/users.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    name: 'Agung',
+    lastName: 'Putra',
+    statusText: 'Valdo',
+    // image: 'users/rick.jpg'
+  },
+  {
+    name: 'Badu',
+    lastName: 'Ntah 1',
+    statusText: 'Apalah',
+    // image: 'users/daryl.jpg'
+  },
+  {
+    name: 'Budi',
+    lastName: 'Ntah 2',
+    statusText: 'Nggak ada status',
+    // image: 'users/carol.jpg'
+  },
+  {
+    name: 'Wati',
+    lastName: 'Ntah 3',
+    statusText: 'Nggak Jelas',
+    // image: 'users/negan.jpeg'
+  }
+]
+
